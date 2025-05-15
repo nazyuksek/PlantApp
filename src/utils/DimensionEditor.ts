635c5200f156e0128ae56209229f2ee6af6782c1
naz <nyuksek17@ku.edu.tr>
2025-05-15 19:00:06 +0300

    minor: dimension editor implemented

---
+++ src/utils/DimensionEditor.ts
@@ -0,0 +1,35 @@
+import {Dimensions} from 'react-native';
+
+const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
+
+/**
+ * A custom class implemented for responsive designs.
+ * Takes 375x812 screen (given in Figma) as base, and calculates width and height for given number for current screen
+ */
+class DimensionEditor {
+  protected SCALE_FACTOR_HORIZONTAL: number;
+  protected SCALE_FACTOR_VERTICAL: number;
+
+  constructor(screenWidth: number, screenHeight: number) {
+    this.SCALE_FACTOR_HORIZONTAL = screenWidth / 375;
+    this.SCALE_FACTOR_VERTICAL = screenHeight / 812;
+  }
+
+  scaleWidth = (width: number) => {
+    return width * this.SCALE_FACTOR_HORIZONTAL;
+  };
+
+  scaleHeight = (height: number) => {
+    return height * this.SCALE_FACTOR_VERTICAL;
+  };
+}
+
+const ParamTechDimensionEditor = new DimensionEditor(
+  SCREEN_WIDTH,
+  SCREEN_HEIGHT,
+);
+
+export const scaleWidth = ParamTechDimensionEditor.scaleWidth;
+export const scaleHeight = ParamTechDimensionEditor.scaleHeight;
+
+export {SCREEN_HEIGHT, SCREEN_WIDTH, ParamTechDimensionEditor};
