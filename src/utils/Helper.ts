1da55c655d65731162fcc00487393f288a8621ab
naz <nyuksek17@ku.edu.tr>
2025-05-17 18:47:44 +0300

    minor: text according to time implemented

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -3,3 +3,18 @@ export const capitalizeFirstChar = (inputString: string) =>
 
 export const removeDash = (inputString: string) =>
   inputString.replaceAll('-', ' ');
+
+export const getTimeBasedEmojiAndTitle = () => {
+  const now = new Date();
+  const hour = now.getHours();
+
+  if (hour >= 5 && hour < 12) {
+    return 'Good morning 🌞';
+  } else if (hour >= 12 && hour < 17) {
+    return 'Good afternoon ⛅︎';
+  } else if (hour >= 17 && hour < 21) {
+    return 'Good evening 🌆';
+  } else {
+    return 'Good night 🌙';
+  }
+};
